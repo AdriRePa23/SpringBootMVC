174ccbba98d3f4a9f02bf35d9f3c002531945b53
adrian <adrianreal234@gmail.com>
2025-01-18 23:43:50 +0100

    Proyecto creado

---
+++ src/test/java/com/areapal/springmvc/SpringBootMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.areapal.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
